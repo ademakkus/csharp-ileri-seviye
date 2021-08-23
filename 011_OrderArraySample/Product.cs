1e84b4ea41a0f5b5e7a2aa11923e35f1b98a6b2b
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-08-23 11:53:31 +0300

    Add files via upload

---
+++ 011_OrderArraySample/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_OrderArraySample
+{
+  public class Product : IOrderDetail
+  {
+    //private int quantity;
+    public int Quantity { get; set; }
+    public double Cost { get; set; }
+
+    public virtual double GetTotalCost()
+    {
+      //throw new NotImplementedException();
+      return Cost * Quantity;
+    }
+  }
+  class Elma : Product
+  { }
+  class Altın : Product
+  {
+    public override double GetTotalCost()
+    {
+      return base.Cost * base.Quantity * 2;
+    }
+  }
+
+  public class Order
+  {
+    ArrayList products = new ArrayList();
+    public void AddProduct(Product urun)
+    {
+      products.Add(urun);
+    }
+    public double GetGrandTotal()
+    {
+      double toplam = 0;
+      foreach (Product _urun in products)
+      {
+       toplam += _urun.GetTotalCost();
+      }
+      return toplam;
+    }
+    public override string ToString()
+    {
+      return this.GetGrandTotal().ToString();
+    }
+  }
+}
+ 
---
+++ 011_OrderArraySample/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_OrderArraySample
+{
+  public class OrderComparer : IComparer
+  {
+    public int Compare(object x, object y)
+    {
+      //throw new NotImplementedException();
+      if (x is Order && y is Order)
+      {
+        Order p1 = (Order)x;
+        Order p2 = (Order)y;
+        if (p1.GetGrandTotal() > p2.GetGrandTotal())
+          return 1;
+        else if (p1.GetGrandTotal() < p2.GetGrandTotal())
+          return -1;
+        else
+          return 0;
+
+      }
+      else
+        throw new ArgumentException();
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Order order1 = new Order();
+      Elma e1 = new Elma();
+      e1.Cost = 10;
+      e1.Quantity = 10;
+      Altın a1 = new Altın();
+      a1.Cost = 100;
+      a1.Quantity = 2;
+      order1.AddProduct(e1);
+      order1.AddProduct(a1);
+      Console.WriteLine(order1.GetGrandTotal());
+      Order order2 = new Order();
+      Elma e2 = new Elma();
+      e2.Cost = 20;
+      e2.Quantity = 10;
+      Altın a2 = new Altın();
+      a2.Cost = 400;
+      a2.Quantity = 2;
+      order2.AddProduct(e2);
+      order2.AddProduct(a2);
+
+      ArrayList siparisler = new ArrayList();
+      siparisler.Add(order1);
+      siparisler.Add(order2);
+      siparisler.Sort(new OrderComparer());
+      foreach (Order order in siparisler)
+      {
+        Console.WriteLine(order.ToString());
+      }
+
+      Console.ReadKey();
+
+
+    }
+  }
+}
